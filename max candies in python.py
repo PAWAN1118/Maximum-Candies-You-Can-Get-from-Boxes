3c474cd74c6190f8847a436bd0a5886502bbfe98
PAWAN1118 <nimmakayalapawankumar7@gmail.com>
2025-06-03 16:07:55 +0530

    max candies in python.py

--- max candies in python.py
+++ max candies in python.py
@@ -1,36 +1,35 @@
 from collections import deque
 
-def maxCandies(status, candies, keys, containedBoxes, initialBoxes):
-    n = len(status)
-    queue = deque(initialBoxes)
-    have_keys = set()
-    have_boxes = set(initialBoxes)
-    visited = set()
-    total_candies = 0
+class Solution:
+    def maxCandies(self, status, candies, keys, containedBoxes, initialBoxes):
+        n = len(status)
+        queue = deque(initialBoxes)
+        have_keys = set()
+        have_boxes = set(initialBoxes)
+        visited = set()
+        total_candies = 0
 
-    while queue:
-        box = queue.popleft()
-        if box in visited:
-            continue
+        while queue:
+            box = queue.popleft()
+            if box in visited:
+                continue
 
-        if status[box] == 1 or box in have_keys:
-            visited.add(box)
-            total_candies += candies[box]
+            if status[box] == 1 or box in have_keys:
+                visited.add(box)
+                total_candies += candies[box]
 
-            # Gain new keys and boxes
-            for k in keys[box]:
-                if k not in have_keys:
-                    have_keys.add(k)
-                    if k in have_boxes and k not in visited:
-                        queue.append(k)
+                for k in keys[box]:
+                    if k not in have_keys:
+                        have_keys.add(k)
+                        if k in have_boxes and k not in visited:
+                            queue.append(k)
 
-            for b in containedBoxes[box]:
-                if b not in have_boxes:
-                    have_boxes.add(b)
-                if status[b] == 1 or b in have_keys:
-                    queue.append(b)
-        else:
-            # We cannot open the box yet, re-queue it for later
-            queue.append(box)
+                for b in containedBoxes[box]:
+                    if b not in have_boxes:
+                        have_boxes.add(b)
+                    if status[b] == 1 or b in have_keys:
+                        queue.append(b)
+            else:
+                queue.append(box)
 
-    return total_candies
+        return total_candies
